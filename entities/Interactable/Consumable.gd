5fd647ea7a78bbd9e74db33b4b1cfae3874cdfc0
Scott Gill <sjgill851@gmail.com>
2023-11-13 07:53:25 -0600

    Fundamental Properties Defined
    I have yet to hook in to the game in any way, but we now have the basic
    properties of these interactables.

--- entities/Interactable/Consumable.gd
+++ entities/Interactable/Consumable.gd
@@ -1,11 +1,17 @@
 extends Interactable
 
-
+var fixable : bool = false
+var usable : bool = true
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	super()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	super(delta)
+
+#Consumables are usually used once, so we unset the flag to tell it to ignore
+#any future interactions.
+func interact():
+	usable = false
--- entities/Interactable/Fixture.gd
+++ entities/Interactable/Fixture.gd
@@ -1,11 +1,16 @@
 extends Interactable
 
-
+var status_tags : PackedStringArray
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	super()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	super(delta)
+
+#Fixtures have ongoing interactions. As of yet, it is unclear how to
+#implement this particular interaction.
+func interact():
 	pass
