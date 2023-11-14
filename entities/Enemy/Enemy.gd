449b39f400bef9d9608c0d078d1aa081bebd04a5
sjgillGit <150327164+sjgillGit@users.noreply.github.com>
2023-11-13 19:57:25 -0600

    Clockin (#9)
    * Restoration and startup
    
    * New element types substantiated
    
    They aren't ready to go yet, but they now exist.
    
    * Template created
    
    Interactable is about as well as it's going to get before being hooked
    into the game script.
    
    * Fundamental Properties Defined
    
    I have yet to hook in to the game in any way, but we now have the basic
    properties of these interactables.
    
    * Establish game states and realize new problems
    
    The enemy game states are now integral to the music system, and I've
    just realized that because all three types of interactables could be
    present in a singular object, I need to rethink how I'm going to code
    interactables.
    
    * Properties of classes rolled back into superclass
    
    Objects can only hold 1 script, and game objects are capable of
    exhibiting properties of all 3 objects. Time to roll it back up.
    
    * Forgot to save the superclass before committing deletes, sorry!
    
    * Only reset the permeable if it's supposed to be resettable.

--- entities/Enemy/Enemy.gd
+++ entities/Enemy/Enemy.gd
@@ -1,12 +1,16 @@
 extends Unit
 
+enum game_state {Patrolling, Suspicious, Chasing}
+
+@export var start_state = game_state
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	move_speed = 24
 	super()
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	super(delta)
+	
+func game_changed(state):
+	start_state = state
