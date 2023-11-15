c8f2a73de10a372e4886301616b2bbc59979fecf
Matthieu Daulhiac <96766017+MatthieuD134@users.noreply.github.com>
2023-11-15 10:36:41 +0800

    small fixes and base enemy node

--- entities/units/enemy/enemy.gd
+++ entities/units/enemy/enemy.gd
@@ -1,11 +1,12 @@
+class_name Enemy
 extends Unit
 
 #Most interactions define where the enemy is going to move. Instead of coding a
 #lot of interactions here in the enemy script, we just want to make sure the
 #enemy can give necessary information to tell the gameboard where to put things.
-enum game_state {Patrolling, Suspicious, Chasing}
+enum game_state {PATROLLING, SUSPICIOUS, CHASING}
 
-@export var start_state = game_state
+@export var current_state := game_state.PATROLLING
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -15,10 +16,10 @@ func _ready():
 func _process(delta):
 	super(delta)
 	
-func game_changed(state):
-	start_state = state
+func update_game_state(state : game_state):
+	current_state = state
 
 #The variable name start_state fits a lot of cases, but can be confusing to
 #reference directly, so this function makes for a more readable alternative.
 func is_in_state(state : game_state) -> bool:
-	return state == start_state
+	return current_state == state
---
+++ entities/units/player/player.gd
@@ -0,0 +1,14 @@
+class_name Player
+extends Unit
+
+
+@onready var is_hidden : bool = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	super()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	super(delta)
