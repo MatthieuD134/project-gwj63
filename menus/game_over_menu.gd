8eb4470e2e4a6c817d2000b063c91bdcef33238f
sjgillGit <150327164+sjgillGit@users.noreply.github.com>
2023-11-19 08:32:54 -0600

    Clockin8 (#41)
    * Enemy Art and Animation done. Splash Menu (background and foreground)
    imported
    
    * Asset placeholders, with some implementation.
    
    * enhance menus (#40)
    
    * First Game Over state implemented
    
    * Laser Pointer animated and balanced
    
    ---------
    
    Co-authored-by: Matthieu Daulhiac <96766017+MatthieuD134@users.noreply.github.com>

--- menus/game_over_menu.gd
+++ menus/game_over_menu.gd
@@ -1,6 +1,7 @@
 extends Control
 
 @onready var badCat : String = "Cuddle Time"
+@onready var badCatI : TextureRect = $CuddleTime
 
 func _ready():
 	self.hide()
@@ -8,7 +9,9 @@ func _ready():
 func _on_main_menu_button_pressed():
 	get_tree().set_pause(false)
 	get_tree().change_scene_to_file("res://menus/main_menu.tscn")
+	badCatI.visible = false
 	badCat = "Cuddle Time"
+	badCatI = $CuddleTime
 
 
 func _on_quit_button_pressed():
@@ -21,12 +24,16 @@ func game_over(infamy, infamyMax) -> void:
 	#print(infamy)
 	if (infamy >= infamyMax):
 		badCat = "FINE! YOU WIN!"
+		badCatI = $Win
 	else:
 		if ((infamy/infamyMax) > 0.10):
 			badCat = "No! Bad Cat!"
+			badCatI = $Spray
 		if ((infamy/infamyMax) > 0.50):
 			badCat = "I GOT YOU NOW! \nYOU ARE A VERY BAD CAT"
+			badCatI = $Anger
 	get_node("PauseMenuContainer/VBoxContainer2/Label").text = badCat
+	badCatI.visible = true
 	self.show()
 	get_tree().set_pause(true)
 	initialize_focus()
