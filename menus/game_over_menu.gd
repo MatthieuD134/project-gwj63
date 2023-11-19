fe879269758a48b08f844c2bfb84c973229a4191
Scott Gill <sjgill851@gmail.com>
2023-11-19 14:07:18 -0600

    This is our Jam release version! Pens and Pencils down!

--- menus/game_over_menu.gd
+++ menus/game_over_menu.gd
@@ -1,6 +1,6 @@
 extends Control
 
-@onready var badCat : String = "Cuddle Time"
+#@onready var badCat : String = "Cuddle Time"
 @onready var badCatI : TextureRect = $CuddleTime
 
 func _ready():
@@ -10,7 +10,7 @@ func _on_main_menu_button_pressed():
 	get_tree().set_pause(false)
 	get_tree().change_scene_to_file("res://menus/main_menu.tscn")
 	badCatI.visible = false
-	badCat = "Cuddle Time"
+#	badCat = "Cuddle Time"
 	badCatI = $CuddleTime
 
 
@@ -23,16 +23,16 @@ func initialize_focus() -> void:
 func game_over(infamy, infamyMax) -> void:
 	#print(infamy)
 	if (infamy >= infamyMax):
-		badCat = "FINE! YOU WIN!"
+#		badCat = "FINE! YOU WIN!"
 		badCatI = $Win
 	else:
 		if ((infamy/infamyMax) > 0.10):
-			badCat = "No! Bad Cat!"
+#			badCat = "No! Bad Cat!"
 			badCatI = $Spray
 		if ((infamy/infamyMax) > 0.50):
-			badCat = "I GOT YOU NOW! \nYOU ARE A VERY BAD CAT"
+#			badCat = "I GOT YOU NOW! \nYOU ARE A VERY BAD CAT"
 			badCatI = $Anger
-	get_node("PauseMenuContainer/VBoxContainer2/Label").text = badCat
+#	get_node("PauseMenuContainer/VBoxContainer2/Label").text = badCat
 	badCatI.visible = true
 	self.show()
 	get_tree().set_pause(true)
