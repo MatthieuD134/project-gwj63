cd4f96ff4162e389199b06e1b1ac4d6bdc1b3b38
Matthieu Daulhiac <96766017+MatthieuD134@users.noreply.github.com>
2023-11-19 19:26:10 +0800

    enhance menus (#40)

---
+++ menus/simple-button.gd
@@ -0,0 +1,24 @@
+extends TextureButton
+
+
+
+func _on_focus_entered():
+	var tween = get_tree().create_tween()
+	tween.tween_property(self, "scale", Vector2(1.1,1.1), 0.2 )
+
+
+func _on_focus_exited():
+	var tween = get_tree().create_tween()
+	tween.tween_property(self, "scale", Vector2(1,1), 0.2 )
+
+
+func _on_mouse_entered():
+	var tween = get_tree().create_tween()
+	tween.tween_property(self, "scale", Vector2(1.1,1.1), 0.2 )
+
+
+
+func _on_mouse_exited():
+	if not self.has_focus():
+		var tween = get_tree().create_tween()
+		tween.tween_property(self, "scale", Vector2(1,1), 0.2 )
