4c632a7ac4dc5ffc208d69df2ba36edc660ce1a1
Matthieu Daulhiac <96766017+MatthieuD134@users.noreply.github.com>
2023-11-20 01:17:04 +0800

    add the ONE option (#46)

--- menus/options_menu.gd
+++ menus/options_menu.gd
@@ -2,9 +2,16 @@ extends Control
 
 signal back_button_pressed()
 
+var master_bus := AudioServer.get_bus_index("Master")
+var MIN := -30
+var MAX := 0
+
 
 func _ready():
 	initialize_focus()
+	$MarginContainer/VBoxContainer/Label/HSlider.set_min(MIN)
+	$MarginContainer/VBoxContainer/Label/HSlider.set_max(MAX)
+	
 
 func _process(_delta):
 	if Input.is_action_just_pressed("ui_cancel"):
@@ -18,4 +25,9 @@ func _on_back_button_pressed():
 # Utility functions
 # -----------------
 func initialize_focus() -> void:
-	$MarginContainer/VBoxContainer/BackButton.grab_focus()
+	$MarginContainer/VBoxContainer/Label/HSlider.grab_focus()
+
+
+func _on_h_slider_value_changed(value):
+	AudioServer.set_bus_volume_db(master_bus, value)
+	AudioServer.set_bus_mute(master_bus, value == MIN)
