ff96423dda9086381ff1e9efb7d8cff230dd6621
Scott Gill <sjgill851@gmail.com>
2023-11-19 08:32:40 -0600

    Commented out restriction on Start Button

--- menus/simple-button.gd
+++ menus/simple-button.gd
@@ -19,6 +19,7 @@ func _on_mouse_entered():
 
 
 func _on_mouse_exited():
-	if not self.has_focus():
-		var tween = get_tree().create_tween()
-		tween.tween_property(self, "scale", Vector2(1,1), 0.2 )
+	#if not self.has_focus():
+	#print("Shrinking Now")
+	var tween = get_tree().create_tween()
+	tween.tween_property(self, "scale", Vector2(1,1), 0.2 )
